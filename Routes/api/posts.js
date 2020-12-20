afeeda634a4947d9f4d133bbe2f2693299b1dd73
Pradyumna Gupta <pradyg29@gmail.com>
2020-12-20 14:47:01 +0530

    Posts Api routes part-2

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -1,9 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Load post model
+const Post = require("../../models/Post");
+
+// Validate posts
+const validatePostInput = require("../../validaton/post");
 
 // @route  GET api/posts/test
 // @de3sc  test post route
 // @access Public
-router.get('/test', (req, res)=> res.json({msg: "Posts work"}));
+router.get("/test", (req, res) => res.json({ msg: "Posts work" }));
+
+// @route  POST api/posts
+// @de3sc  create post
+// @access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) {
+      //return error with 400 status
+      res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id,
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
