9869be21d3916b7656aee62380e3eda546926a91
Pradyumna Gupta <pradyg29@gmail.com>
2020-12-20 18:43:34 +0530

    Post api routes

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 
 //Load post model
 const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
 
 // Validate posts
 const validatePostInput = require("../../validaton/post");
@@ -13,6 +14,27 @@ const validatePostInput = require("../../validaton/post");
 // @de3sc  test post route
 // @access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts work" }));
+
+// @route  get api/posts
+// @de3sc  get post
+// @access Public
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json({ nopost: "no post found" }));
+});
+
+// @route  get api/posts/:id
+// @de3sc  get post by id
+// @access Public
+router.get("/:id", (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err =>
+      res.status(404).json({ nopostfound: "no post found with that id" })
+    );
+});
 
 // @route  POST api/posts
 // @de3sc  create post
@@ -35,6 +57,31 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route  DELETE api/posts/:id
+// @de3sc  delete post
+// @access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // check for post owner
+          if (post.user.toString() !== req.user.id) {
+            return res
+              .status(401)
+              .json({ notauthorized: "User not authorized" });
+          }
+
+          // Delete
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err => res.status(404).json({ postnotfound: "Post not found" }));
+    });
   }
 );
 
