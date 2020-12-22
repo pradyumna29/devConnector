f322cbff1538bc744ca3bb2d828507423768ed08
Pradyumna Gupta <pradyg29@gmail.com>
2020-12-22 18:40:05 +0530

    React part-3

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+
+import NavBar from "./Components/layout/NavBar";
+import Footer from "./Components/layout/Footer";
+import Landing from "./Components/layout/Landing";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>My React App</h1>
+        <NavBar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
