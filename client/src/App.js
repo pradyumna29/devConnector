b945f2e94ab114f71eb1401bf1a5afb717ed308a
Pradyumna Gupta <pradyg29@gmail.com>
2020-12-23 18:41:03 +0530

    add error in registration form and redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import NavBar from "./Components/layout/NavBar";
 import Footer from "./Components/layout/Footer";
 import Landing from "./Components/layout/Landing";
@@ -12,17 +13,19 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <NavBar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <NavBar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
