fbe2ab1b54ba9360e9d37b28a83ca7e16b6f6075
Pradyumna Gupta <pradyg29@gmail.com>
2020-12-24 22:30:14 +0530

    spinner component dashboard start

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // LogoutUser
     store.dispatch(logoutUser());
-    // TODO: clear current profile
+    // clear current profile
     store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
